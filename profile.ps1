b777fce7b2ccadc55d0477e1af44c41f283a8216
Willem Toorenburgh <50425141+WillemToorenburgh@users.noreply.github.com>
2024-03-28 09:41:05 -0700

    Updating profile
    * PowerToys' WinGetCommandNotFound module
    * Conditional registration of the `az` argument completer
    * Disabling of the MenuComplete PSReadlineKeyHandler
    Note: these have been in the profile for a while but I forgot to commit them

--- profile.ps1
+++ profile.ps1
@@ -1,4 +1,7 @@
-#This is the $PROFILE.CurrentUserAllHosts when run from Windows Terminal
+#This is the $PROFILE when run from Windows Terminal
+
+# Get Perforce to set up env vars
+# p4 set
 
 New-Alias -Name 'll' -Value 'ls'
 New-Alias -Name 'which' -Value 'Get-Command'
@@ -7,14 +10,41 @@ New-Alias -name 'dig' -Value 'Resolve-DNSName'
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
+    Import-Module "$ChocolateyProfile"
 }
 
-# Enable if working on Azure
-# Import-Module Az.Tools.Predictor
+Import-Module Az.Tools.Predictor
 Import-Module Terminal-Icons
 Import-Module posh-git
 Import-Module CompletionPredictor
+
+#34de4b3d-13a8-4540-b76d-b9e8d3851756 PowerToys CommandNotFound module
+
+Import-Module "C:\Program Files\PowerToys\WinUI3Apps\..\WinGetCommandNotFound.psd1"
+#34de4b3d-13a8-4540-b76d-b9e8d3851756
+
+If (Get-Command az) {
+    Register-ArgumentCompleter -Native -CommandName az -ScriptBlock {
+        param($commandName, $wordToComplete, $cursorPosition)
+        $completion_file = New-TemporaryFile
+        $env:ARGCOMPLETE_USE_TEMPFILES = 1
+        $env:_ARGCOMPLETE_STDOUT_FILENAME = $completion_file
+        $env:COMP_LINE = $wordToComplete
+        $env:COMP_POINT = $cursorPosition
+        $env:_ARGCOMPLETE = 1
+        $env:_ARGCOMPLETE_SUPPRESS_SPACE = 0
+        $env:_ARGCOMPLETE_IFS = "`n"
+        $env:_ARGCOMPLETE_SHELL = 'powershell'
+        az 2>&1 | Out-Null
+        Get-Content $completion_file | Sort-Object | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, "ParameterValue", $_)
+        }
+        Remove-Item $completion_file, Env:\_ARGCOMPLETE_STDOUT_FILENAME, Env:\ARGCOMPLETE_USE_TEMPFILES, Env:\COMP_LINE, Env:\COMP_POINT, Env:\_ARGCOMPLETE, Env:\_ARGCOMPLETE_SUPPRESS_SPACE, Env:\_ARGCOMPLETE_IFS, Env:\_ARGCOMPLETE_SHELL
+    }
+}
+
+# Disabling as it seems to be causing issues with the VSCode Powershell extension integrated terminal
+# Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\kushal.omp.json" | Invoke-Expression
 $env:POSH_GIT_ENABLED = $true
